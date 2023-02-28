ae3df83e48f9606614d3430e8f801fbc72eb5a75
Dylan Hackworth <followmelikethunder@pm.me>
2023-02-28 03:08:53 +0000

    bug squash

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,5 @@
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/rest/v9";
-
 import { GuildAuditLogs } from "discord.js";
-import { commandFiles } from "../files";
+import { getCommandFiles } from "../files";
 import { Bot, BotCommand } from "../structures";
 import { TypedEvent } from "../types";
 import modmailCmds from "../services/modmail";
@@ -13,44 +10,31 @@ export default TypedEvent({
     run: async (client: Bot) => {
         client.logger.console.info(`Logged in as ${client.user?.tag}.`);
 
+        // register our slash commands
+        const commandFiles = getCommandFiles();
         const commandArr: BotCommand[] = [
-            // HACK(dylhack): this is a little hack to get modmail up and
+            // NOTE(dylhack): this is a little hack to get modmail up and
             //                working. it's possibly not a preferable way of
             //                doing this.
             ...modmailCmds(),
         ];
 
-        let tasks: Promise<unknown>[] = [];
-        for (let i = 0; i < commandFiles.length; i += 1) {
-            const file = commandFiles[i];
-            const task = import(file);
-            task.then((module) => {
-                const command = module.default as BotCommand;
-                if (command === undefined) {
-                    console.error(
-                        `File at path ${file} seems to incorrectly` +
-                            " be exporting a command."
-                    );
-                } else {
-                    commandArr.push(command);
-                }
-            });
-            tasks.push(task);
-        }
+        const modules = await Promise.all(
+            commandFiles.map((file) => import(file))
+        );
+        modules.forEach((module) => {
+            const command = module.default as BotCommand;
+            if (command === undefined) {
+                return;
+            } else {
+                commandArr.push(command);
+            }
+        });
 
-        await Promise.all(tasks);
+        await client.register(commandArr);
 
-        for (let i = 0; i < commandArr.length; i += 1) {
-            const command = commandArr[i];
-            client.commands.set(command.data.name, command);
-            client.logger.console.debug(
-                `Registered command ${command.data.name}`
-            );
-        }
-
-        const payload = commandArr.map((cmd) => cmd.data);
-
-        tasks = [];
+        // do some audit log stuff
+        const tasks: Promise<unknown>[] = [];
         client.guilds.cache.forEach((guild) => {
             const task = guild
                 .fetchAuditLogs({
@@ -67,34 +51,6 @@ export default TypedEvent({
             tasks.push(task);
         });
         await Promise.all(tasks);
-
-        const rest = new REST({ version: "9" }).setToken(
-            process.env.TOKEN || ""
-        );
-
-        // Register to a testing server
-        const devServer = process.env.DEV_SERVER;
-        if (devServer !== undefined) {
-            await rest.put(
-                Routes.applicationGuildCommands(client.user.id, devServer),
-                { body: payload }
-            );
-            client.logger.console.info(`Registered commands to ${devServer}`);
-            return;
-        }
-        // else... register globally
-
-        // clear dev commands
-        tasks = [];
-        client.guilds.cache.forEach((guild) => {
-            const task = guild.commands.set([]);
-            tasks.push(task);
-        });
-        await Promise.all(tasks).catch(() => null);
-        // register global commands
-        await rest.put(Routes.applicationCommands(client.user.id), {
-            body: payload,
-        });
-        client.logger.console.info("Registered commands globally");
+        client.logger.console.info("Ready");
     },
 });
